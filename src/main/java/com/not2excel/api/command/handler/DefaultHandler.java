3472b75ddd7a8532ab1e89177d9e9c2c2d90b85c
kh498 <klink100@gmail.com>
2017-06-18 21:04:35 +0200

    Disable check for unknown subcommands
    When you think about it, it was kinda dumb... the commands couldn't have any variables them self..

--- src/main/java/com/not2excel/api/command/handler/DefaultHandler.java
+++ src/main/java/com/not2excel/api/command/handler/DefaultHandler.java
@@ -103,13 +103,13 @@ public class DefaultHandler implements Handler {
             return;
         }
 
-        if (info.getArgsLength() > 0) {
-            final String arg0 = info.getArgs().get(0);
-            if (!info.getParentCommand().hasChild(arg0) && !CommandInfo.isValidFlag(arg0)) {
-                sendHelpScreen(info, "Unknown subcommand: " + arg0);
-                return;
-            }
-        }
+//        if (info.getArgsLength() > 0) {
+//            final String arg0 = info.getArgs().get(0);
+//            if (!info.getParentCommand().hasChild(arg0) && !CommandInfo.isValidFlag(arg0)) {
+//                sendHelpScreen(info, "Unknown subcommand: " + arg0);
+//                return;
+//            }
+//        }
         if (!info.hasAsteriskFlag()) {
             for (final char flag : info.getFlags()) {
                 if (!ch.flags().contains(String.valueOf(flag))) {
